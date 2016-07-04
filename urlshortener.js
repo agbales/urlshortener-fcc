02695767512b4f1924afc5044c24b44239b0e06b
Andrew <agbales@gmail.com>
2016-07-04 00:40:33 +0000

    init commit

---
+++ urlshortener.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+	process.on('uncaughtException', function (err) {
+	    console.log(err);
+	}); 
+});
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+'/public/index.html'));
+})
+
+app.get('/:query', function(req, res) {
+
+	res.send('this');
+});
