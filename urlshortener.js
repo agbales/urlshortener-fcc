50822f6a7f040a3c6559626f6792eb0142c275c8
Andrew <agbales@gmail.com>
2016-07-06 05:29:57 +0000

    listing numbering and insertion

--- urlshortener.js
+++ urlshortener.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var path = require('path');
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var url = 'mongodb://localhost:27017/urlshortener';
 var app = express();
-
 var port = process.env.PORT || 8080;
+
 
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
@@ -11,10 +14,74 @@ app.listen(port,  function () {
 	}); 
 });
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname+'/public/index.html'));
-})
+});
+
+app.get('/urls', function(req, res){
+   MongoClient.connect(url, function(err, db) {
+      if (err) throw err
+      console.log("connected to mongo")
+      var collection = db.collection('urls');
+      collection.find({}).toArray(function(err, result) {
+         if (err) throw err 
+         res.send(result);
+        db.close()
+      });
+   });
+});
+
+app.get('/:query', function(req, res) {
+    var query = req.params.query
+    // if collection ID ends in query, Redirect.
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err
+        console.log('Connection established to', url);
+        
+        db.collection('urls').find({
+            short : query
+        }).toArray(function(err, docs) {
+            if (err) throw err
+            res.redirect(docs[0].original)
+            db.close()
+        })
+    })
+});
 
 app.get('/http(s)?://*', function(req, res) {
-	res.send(req.params[1]);
-});
+    var fullUrl = req.url.slice(1);
+    var listing;
+    
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err
+        console.log('Connection established to', url);
+    
+        var shortUrl;
+        db.collection('urls').count()
+                            .then(function(num) {
+                                if (err) throw err;
+                                listing = {
+                                    original : fullUrl,
+                                    short: num.toString()
+                                }
+                                console.log(listing);
+                                insertListing(err, listing);
+                            });
+                            
+        function insertListing() {
+            var collection = db.collection('urls');
+            collection.insert(listing, function(err, data) {
+                if (err) throw err
+                console.log('INSERTED' + data);
+                db.close();
+            });
+        }
+        
+        res.redirect('/urls');
+    });
+
+});   
+        
+
+
